f60c226f133d0a34fc33c764a8a5e924d0e4f004
Thomas <thomas.bennett3@my.jcu.edu.au>
2019-08-20 09:59:19 +1000

    online tests one and two

---
+++ online_test_1.py
@@ -0,0 +1,65 @@
+# number = 15 / 3 + 2 * 4
+#
+# print(number)
+
+# number = 7 % 3
+#
+# print(number)
+
+# x = 10
+# while x > 4:
+#     print(x, end=" ")
+#     x = x - 2
+
+# nums1 = [1, -5, 2, 0, 4, 2, -3]
+# nums2 = [1, -5, 2, 4, 4, 2, 7]
+# result = 0
+# j = 0
+# while j < len(nums1):
+#     if nums1[j] != nums2[j]:
+#         result = result + 1
+#     j = j + 1
+#     print(result)
+
+# x = 5
+# for i in range(x):
+#     x = x + i
+#     print(x, end=" ")
+
+# for i in range(1, 10, 4):
+#     print(i, i * 2, end=" ")
+
+# a = 5
+# b = 10
+# c = 0.5
+#
+# number = a == b * c or c * 10 >= a
+#
+# print(number)
+
+# x = 1
+# y = 2
+# z = 3
+# if x < y:
+#     if y > 4:
+#         z = 5
+#     else:
+#         z = 6
+#
+# print(z)
+
+# print(5 / 2)
+
+# x = 5
+# y = 10
+# if x < 10:
+#     if y > 10 and x > 5:
+#         print("A")
+# else:
+#     print("B")
+
+print(5 // 2)
+
+
+
+
---
+++ online_test_2.py
@@ -0,0 +1,37 @@
+# x = str(1.0)
+# x[-1] = '2'
+
+
+# text = "Enjoy the test"
+# result = text.strip().split()[0]
+
+
+# x = str(int('1.0'))
+# x[-1] = '2'
+
+
+# try:
+#     x = int("zero")
+#     print(10 / x)
+# except ZeroDivisionError:
+#     print("div")
+# except NameError:
+#     print("name")
+# except ValueError:
+#     print("value")
+# except:
+#     print("other")
+
+
+# a = open(b, c)
+# d = a.read()
+
+
+import math
+
+print(math.pi)
+
+
+
+
+
